a0ef01870a7fdb33fef9c0c12fae8767d5c06723
rukshankalhara <83764243+rukshankalhara@users.noreply.github.com>
2024-12-03 10:53:10 +0530

    Update DevopsIntegrationApplication.java-10

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to Test-Web-1";
+		return "welcome to Test-Web-2";
 	}
 
 	public static void main(String[] args) {
